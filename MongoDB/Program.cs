84f30cc41a970cfa5f0e49816ce5fcdca231a903
Tom Honeyfield <tomhoneyfield@hotmail.com>
2021-10-03 00:41:57 +0100

    Add project files.

---
+++ MongoDB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace MongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var databaseName = "sampledb";
+            var connectionString = "mongodb://Mahone_1985:7f2AHeK06ScpAePs@sampledb-shard-00-02.egd8v.mongodb.net:27017/";
+            var client = new MongoClient(connectionString + databaseName);
+
+
+
+        }
+    }
+}
