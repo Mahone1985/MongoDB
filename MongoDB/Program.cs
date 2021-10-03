cb13cd740bc432798ef9b005e5a1287ef4119a78
w4dd325 <w4dd325@gmail.com>
2021-10-03 17:53:12 +0100

    Code clean up

--- MongoDB/Program.cs
+++ MongoDB/Program.cs
@@ -8,56 +8,25 @@ namespace MongoDB
     {
         static void Main(string[] args)
         {
-
-            //var databaseName = "sampledb";
-            //var connectionString = "mongodb://Mahone_1985:7f2AHeK06ScpAePs@sampledb-shard-00-02.egd8v.mongodb.net:27017/";
-            //var client = new MongoClient(connectionString + databaseName);
-
-            //var GetTypeVar = client.GetType();
-            //Console.WriteLine("collection = {0}", GetTypeVar);
-
-            //var ClusterIdVar = client.Cluster.ClusterId.ToString();
-            //Console.WriteLine("collection = {0}", ClusterIdVar);
-
-            //var ListDatabasesVar = client.GetDatabase(databaseName);
-            //Console.WriteLine("collection = {0}", ListDatabasesVar);
-
-            //var dbList = client.ListDatabaseNames().ToList();
-
-            //Console.WriteLine("The list of databases on this server is: ");
-            //foreach (var db in dbList)
-            //{
-            //    Console.WriteLine(db);
-            //}
-
-            //var dbList = client.ListDatabases().ToList();
-
-            //Console.WriteLine("The list of databases on this server is: ");
-            //foreach (var db in dbList)
-            //{
-            //    Console.WriteLine(db);
-            //}
-
-            //var GetTypeVar = client.GetType();
-            //Console.WriteLine("collection = {0}", GetTypeVar);
-
-            //var ClusterIdVar = client.Cluster.ClusterId.ToString();
-            //Console.WriteLine("collection = {0}", ClusterIdVar);
-
-            //var ListDatabasesVar = client.GetDatabase(databaseName);
-            //Console.WriteLine("collection = {0}", ListDatabasesVar);
-
-
-            //////////////////////////////////////////// Wadders
-            ///
-
-                        //NOTES:
-            //Cluster name = myFirstDatabase
+            //NOTES:
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //My DB details
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Wadders_88 | d7BlSqKCqqfCHY4Z | mysampledb-shard-00-02.xcrwt.mongodb.net:27017 | Cluster name = mySampleDB
             //Database name(s) = sample_airbnb | sample_analytics | sample_geospatial | sample_mflix | sample_restaurants | sample_supplies | sample_training | sample_weatherdata
             //Collections (aka tables) = listed under each database ^
+            //Connection string = mongodb+srv://Wadders_88:O2z4HTSyLDrecSeo@mysampledb.xcrwt.mongodb.net/mysampledb?retryWrites=true&w=majority
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Tom's DB details
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Wadders_88 | d7BlSqKCqqfCHY4Z | sampledb-shard-00-02.egd8v.mongodb.net:27017 | Cluster name = sampledb
+            //Database name(s) = sample_airbnb | sample_analytics | sample_geospatial | sample_mflix | sample_restaurants | sample_supplies | sample_training | sample_weatherdata
+            //Collections (aka tables) = listed under each database ^
+            //Connection string = mongodb+srv://Wadders_88:d7BlSqKCqqfCHY4Z@sampledb.egd8v.mongodb.net/sampledb?retryWrites=true&w=majority
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
             //MongoDB connection
-            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Mahone_1985:7f2AHeK06ScpAePs@sampledb.egd8v.mongodb.net/sampledb?retryWrites=true&w=majority");
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Wadders_88:d7BlSqKCqqfCHY4Z@sampledb.egd8v.mongodb.net/sampledb?retryWrites=true&w=majority");
             var client = new MongoClient(settings);
             var database = client.GetDatabase("sample_training");
 
@@ -69,25 +38,30 @@ namespace MongoDB
                 Console.WriteLine(db);
             }
             Console.WriteLine("");
+
+            //BsonDoc for dbstats
+            /*
+            var command = new BsonDocument { { "dbstats", 1 } };
+            var res = database.RunCommand<BsonDocument>(command);
+            Console.WriteLine("dbstats = {0}", res.ToJson());
             Console.WriteLine("");
+            */
 
-            ////BsonDoc for dbstats
-            //var command = new BsonDocument { { "dbstats", 1 } };
-            //var res = database.RunCommand<BsonDocument>(command);
-            //Console.WriteLine("dbstats = {0}", res.ToJson());
-            //Console.WriteLine("");
-            //Console.WriteLine("");
-
+            //BsonDoc with filter for companies called Facebook
+            /*
+            var companies = database.GetCollection<BsonDocument>("companies");
+            var filter = Builders<BsonDocument>.Filter.Eq("name", "Facebook");
+            var doc = companies.Find(filter).FirstOrDefault();
+            Console.WriteLine(doc.ToString());
+            Console.WriteLine("");
+            */
 
             //BsonDoc with filter count for companies called Facebook
-
             var companies = database.GetCollection<BsonDocument>("companies");
             var filter1 = companies.Find(Builders<BsonDocument>.Filter.Eq("name", "Facebook"));
             var filter2 = companies.Find(Builders<BsonDocument>.Filter.Eq("name", "Wetpaint"));
             Console.WriteLine("Companies count = {0}", filter1.CountDocuments() + filter2.CountDocuments());
-
-
-
+            Console.WriteLine("");
 
         }
     }
